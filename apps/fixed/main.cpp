24e11bb72cc89a60de0e892716590abb76793cc6
Shereemorphett <sheree.morphett@gmail.com>
2023-09-04 18:28:22 +0300

    startinf ex02

---
+++ apps/fixed/main.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smorphet <smorphet@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/04 18:27:15 by smorphet          #+#    #+#             */
+/*   Updated: 2023/09/04 18:27:52 by smorphet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+
+//https://www.cprogramming.com/tutorial/floating_point/understanding_floating_point.html
+
+//https://www.cprogramming.com/tutorial/floating_point/understanding_floating_point_representation.html
+
+//https://www.cprogramming.com/tutorial/floating_point/understanding_floating_point_printing.html
+
+//https://inst.eecs.berkeley.edu//~cs61c/sp06/handout/fixedpt.html
+
+//https://en.wikipedia.org/wiki/University_of_California,_Berkeley#Notable_alumni.2C_faculty.2C_and_staff
