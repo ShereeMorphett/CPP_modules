0dee25a4cf02d380d38e0949400cd8a5000c521f
Shereemorphett <sheree.morphett@gmail.com>
2023-09-15 16:52:43 +0300

    fixing the issue with copy constructors

--- apps/ex03/ex02/main.cpp
+++ apps/ex03/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: smorphet <smorphet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 12:18:51 by smorphet          #+#    #+#             */
-/*   Updated: 2023/09/15 16:24:57 by smorphet         ###   ########.fr       */
+/*   Updated: 2023/09/15 16:51:52 by smorphet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,20 @@ int main()
 {
 	ClapTrap ClapTrp("ClapTrap");
 	ClapTrap CpyTrp(ClapTrp);
-	ScavTrap ScavTrap("ScavTrap");
-	FragTrap FragTrap("FragTrap");
+	ScavTrap ScavTrp("ScavTrap");
+	FragTrap FragTrp("FragTrp");
+	ScavTrap CpyScavTrap(ScavTrp);
+	FragTrap CpyFragTrap(FragTrp);
 	
 	CpyTrp.setName("COPYTrap");
 	
 	std::cout << COLOR_BRIGHT_BLUE << "Battle Comences" << COLOR_RESET << std::endl;
-	CpyTrp.attack(ScavTrap.GetName());
-	ScavTrap.takeDamage(CpyTrp.GetAttackDamage());
-	ClapTrp.attack(ScavTrap.GetName());
-	ScavTrap.takeDamage(ClapTrp.GetAttackDamage());
-	ClapTrp.attack(FragTrap.GetName());
-	FragTrap.takeDamage(ClapTrp.GetAttackDamage());
+	CpyTrp.attack(ScavTrp.GetName());
+	ScavTrp.takeDamage(CpyTrp.GetAttackDamage());
+	ClapTrp.attack(ScavTrp.GetName());
+	ScavTrp.takeDamage(ClapTrp.GetAttackDamage());
+	ClapTrp.attack(FragTrp.GetName());
+	FragTrp.takeDamage(ClapTrp.GetAttackDamage());
 
 	std::cout << std::endl << COLOR_BRIGHT_BLUE << "Clap and Cpy trap use repair then attack twice....but did ClapTrp get too keen?" << COLOR_RESET << std::endl;
 	CpyTrp.beRepaired(5);
@@ -38,21 +40,21 @@ int main()
 	// 	ClapTrp.beRepaired(5);
 	
 	
-	ScavTrap.attack(CpyTrp.GetName());
-	CpyTrp.takeDamage(ScavTrap.GetAttackDamage());
-	ScavTrap.attack(ClapTrp.GetName());
-	ClapTrp.takeDamage(ScavTrap.GetAttackDamage());
+	ScavTrp.attack(CpyTrp.GetName());
+	CpyTrp.takeDamage(ScavTrp.GetAttackDamage());
+	ScavTrp.attack(ClapTrp.GetName());
+	ClapTrp.takeDamage(ScavTrp.GetAttackDamage());
 	
-	std::cout << std::endl << COLOR_BRIGHT_BLUE << "ScavTrap and FragTrap use repair then bully dead/tired opponents" << COLOR_RESET << std::endl;
+	std::cout << std::endl << COLOR_BRIGHT_BLUE << "ScavTrp and FragTrp use repair then bully dead/tired opponents" << COLOR_RESET << std::endl;
 //////////////////////new test/////////////////////////////////////
-	FragTrap.beRepaired(50);
-	FragTrap.attack(ClapTrp.GetName());
-	ClapTrp.takeDamage(ScavTrap.GetAttackDamage());
+	FragTrp.beRepaired(50);
+	FragTrp.attack(ClapTrp.GetName());
+	ClapTrp.takeDamage(ScavTrp.GetAttackDamage());
 	
-	ScavTrap.beRepaired(5);	
-	ScavTrap.attack(CpyTrp.GetName());
-	CpyTrp.takeDamage(ScavTrap.GetAttackDamage());
-	ClapTrp.takeDamage(ScavTrap.GetAttackDamage());
+	ScavTrp.beRepaired(5);	
+	ScavTrp.attack(CpyTrp.GetName());
+	CpyTrp.takeDamage(ScavTrp.GetAttackDamage());
+	ClapTrp.takeDamage(ScavTrp.GetAttackDamage());
 	
 	std::cout << std::endl << COLOR_BRIGHT_BLUE << "Clap and Cpy trap try to use repair" << COLOR_RESET << std::endl;
 	CpyTrp.beRepaired(5);
@@ -61,8 +63,8 @@ int main()
 	std::cout << std::endl;
 	
 	std::cout << std::endl << COLOR_BRIGHT_BLUE << "The Battle is over!" << COLOR_RESET << std::endl;
-	ScavTrap.guardGate();
-	FragTrap.highFivesGuys();
+	ScavTrp.guardGate();
+	FragTrp.highFivesGuys();
 	
 	
 	return 0;
