a56a2991550d41eb6212ad9ef8c70f46e82729f1
Shereemorphett <sheree.morphett@gmail.com>
2023-10-11 19:15:35 +0300

    addition of template functions

--- apps/ex06/ex00/ScalarConverter.cpp
+++ apps/ex06/ex00/ScalarConverter.cpp
@@ -3,107 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   ScalarConverter.cpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: smorphet <smorphet@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: smorphet <smorphet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 13:02:18 by smorphet          #+#    #+#             */
-/*   Updated: 2023/10/11 14:43:13 by smorphet         ###   ########.fr       */
+/*   Updated: 2023/10/11 19:13:15 by smorphet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
 #include "ScalarConverter.hpp"
 #include <limits>
-#include <cstring>
 
-
-int ScalarConverter::intValue_ = 0;
-float ScalarConverter::fltValue_ = 0.0f;
-char ScalarConverter::charValue_ = '\0';
-double ScalarConverter::dblValue_ = 0.0;
-int ScalarConverter::type_ = INT; 
-bool ScalarConverter::isPossible_ = true; 
-int ScalarConverter::isPseudo_ = NOTPSEUDO; 
-
-void ScalarConverter::convert(const std::string &input)
+enum e_type
 {
-	try
-	{
-    	setType(input);
-		if (type_ == NOTYPE)
-			throw invalidType();
-		if (isPseudo_ == NOTPSEUDO && isPossible_ == true)
-		{
-			initialConversion(input);
-			if (isPseudo_ == NOTPSEUDO && isPossible_ == true)
-			{
-				initialConversion(input);
-				if (type_ != CHAR)
-					convertToChar();
-				if (type_ != INT)
-					convertToInt();
-				if (type_ != FLOAT)
-					convertToFlt();
-				if (type_ != DOUBLE)
-					convertToDbl();
-			}
-		}
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << '\n';
-		return ;
-	}
-}
+    CHAR, INT, FLOAT, DOUBLE, NOTYPE, NANINF
+};
 
-const char* ScalarConverter::invalidType::what() const throw()
+struct s_state
 {
-    return "There are no valid types for this input";
-}
-
-void ScalarConverter::initialConversion(const std::string &input)
-{
-	switch(type_)
-	{
-		case CHAR:
-			charValue_ = input.front();
-			break;
-		case INT:
-			intValue_ = stoi(input, nullptr);
-			break;
-		case FLOAT:
-			fltValue_ = stof(input, nullptr);
-			break;
-		case DOUBLE:
-			dblValue_ = stod(input, nullptr);
-			break;
-	}
-}
-
-bool ScalarConverter::isInfOrNan(const std::string& input)
-{
-    const char* cstr = input.c_str();
-
-    if (std::strcmp(cstr, "nanf") == 0) //does this print differently?
-	{
-		isPseudo_ = NAN;
-        return true;
-	}
-    else if (std::strcmp(cstr, "nan") == 0)
-    {
-		isPseudo_ = NAN;
-	    return true;
-	}
-    else if (std::strcmp(cstr, "+inf") == 0)
-    {
-		isPseudo_ = POSINF;
-	    return true;
-	}
-    else if (std::strcmp(cstr, "-inf") == 0)
-    {
-		isPseudo_ = NEGINF;
-	    return true;
-	}
-    return false;
-}
+    char charValue;
+    int intValue;
+    double dblValue;
+    float fltValue;
+};
 
 static int decimalCounter(const std::string& input)
 {
@@ -119,74 +41,169 @@ static int decimalCounter(const std::string& input)
     return decCount;
 }
 
-void ScalarConverter::setType(const std::string& input)
+
+// static bool isInfOrNan(const std::string& input)
+// {
+//     const char* cstr = input.c_str();
+
+//     if (input == "nanf") //does this print differently?
+// 	{
+// 		isPseudo_ = NAN;
+//         return true;
+// 	}
+//     else if (input == "nan")
+//     {
+// 		isPseudo_ = NAN;
+// 	    return true;
+// 	}
+//     else if (input == "+inf")
+//     {
+// 		isPseudo_ = POSINF;
+// 	    return true;
+// 	}
+//     else if (input == "-inf")
+//     {
+// 		isPseudo_ = NEGINF;
+// 	    return true;
+// 	}
+//     return false;
+// }
+
+//template function for each type
+
+template <typename T>
+static void printChar(T val)
 {
-	if (input.empty())
+    std::cout << "char: ";
+
+    if (val < static_cast<T>(0) || val >= static_cast<T>(128))
     {
-        type_ = NOTYPE;
-		isPossible_ = false;
+        std::cout << "impossible\n";
+        return;    
+    }
+    
+    if (static_cast<char>(val) <= ' ' || static_cast<char>(val) == 127) // 127 == DEL
+    {
+        std::cout << "Non displayable\n";
+        return;    
+    }
+
+    std::cout << static_cast<char>(val) << "\n";
+}
+
+template <typename T>
+static void printInt(T val)
+{
+    //NEEDS TO CHECK ALL THE THINGS
+    std::cout << "int: ";
+    std::cout << static_cast<int>(val) << "\n";
+}
+
+template <typename Float, typename T>
+static void printFloatingPoint(const char* type_name, T val, const char* suffix)
+{
+    std::cout << type_name << ": ";
+    
+    std::cout << static_cast<Float>(val);
+    if (suffix != NULL)
+    {
+        std::cout << "f\n";
+        return;
+    }
+    std::cout << "\n";
+}
+
+template <typename T>
+static void print(T val)
+{
+    printChar(val);
+    printInt(val);
+    printFloatingPoint<float>("float", val, "f");
+    printFloatingPoint<double>("double", val, NULL);
+}
+
+
+void ScalarConverter::convert(const std::string &input)
+{
+    try
+    {
+        if (input.empty())
+        {
+            throw invalidType();
+        }
+
+        // if (isInfOrNan(input))//
+        // {
+        //     std::cout << "PSUEDO LITERAL DETECTED\n";
+        //     return;
+        // }
+        
+        if (input.length() == 1 && !std::isdigit(input[0]))
+        {
+            print(input[0]);
+            return;
+        }
+        if (/* ends with an f or contains a . */)
+        {
+            // read in as double
+            // do numchars thing, throw if hasnt consumed the whole string
+            
+            return;
+        }
+        else(/*read in as int*/)
+        // do numchars thing, throw if hasnt consumed the whole string
+
+        
+        else
+        {
+            int decimalCount = decimalCounter(input);
+            std::size_t numChars;
+            long double lDConversion = stold(input, &numChars);
+            if (numChars == input.length())
+            {
+                if (input.length() == 1 && !std::isdigit(input[0]))
+                {
+                    char val = input[0];
+                    print(val);
+                }
+                if (decimalCount == 1 && input.back() == 'f' && lDConversion <= std::numeric_limits<float>::max())
+                {
+                    float val = stof(input, nullptr);
+                    print(val);
+                }
+                else if (decimalCount == 0 && lDConversion <= std::numeric_limits<int>::max() && lDConversion >= std::numeric_limits<int>::min())
+                {
+                    int val = stoi(input, nullptr);
+                    print(val);
+                }
+                if (lDConversion <= std::numeric_limits<double>::max())
+                {
+                    int val = stod(input, nullptr);
+                    print(val);
+                }
+                else
+                {
+                    throw invalidType();
+                    return;
+                }
+            }
+            
+        }
+    }
+    catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
 		return ;
-    }
-    else if (isInfOrNan(input))
-    {
-        //what does char do?
-        intValue_ = std::numeric_limits<int>::infinity();
-        fltValue_ = std::numeric_limits<float>::infinity();
-        dblValue_ = std::numeric_limits<double>::infinity();
-    }
-    else if (input.length() == 1 && !std::isdigit(input[0]))
-    {
-        type_ = CHAR;
-    }
-    else
-    {
-        try
-        {
-			int decimalCount = decimalCounter(input);
-
-            long double lDConversion = stold(input, nullptr);
-            if (decimalCount == 1 && input.back() == 'f' && lDConversion <= std::numeric_limits<float>::max())
-            {
-                type_ = FLOAT;
-            }
-            else if (decimalCount == 0 && lDConversion <= std::numeric_limits<int>::max() && lDConversion >= std::numeric_limits<int>::min())
-            {
-                type_ = INT;
-            }
-            if (lDConversion <= std::numeric_limits<double>::max())
-            {
-                type_ = DOUBLE;
-            }
-            else
-            {
-                type_ = NOTYPE;
-            }
-        }
-        catch (const std::exception&)
-        {
-            type_ = NOTYPE;
-        }
-    }
+	}
 }
 
-void ScalarConverter::convertToChar()
+
+
+
+const char* ScalarConverter::invalidType::what() const throw()
 {
-	charValue_ = static_cast<char>();
+    return "There are no valid types for this input";
 }
-
-void ScalarConverter::convertToInt()
-{
-	charValue_ =
-}
-
-void ScalarConverter::convertToFlt()
-{
-}
-
-void ScalarConverter::convertToDbl()
-{
-}
-
 
 ////constructors and copy operator overload////////
 
@@ -201,14 +218,7 @@ ScalarConverter::ScalarConverter( ScalarConverter const & src )
 ScalarConverter::~ScalarConverter()
 {}
 
-ScalarConverter &	ScalarConverter::operator=(ScalarConverter const &right)
+ScalarConverter &	ScalarConverter::operator=(ScalarConverter const &right) //this is useless
 {
-    if (this != &right)
-    {
-		this->charValue_ = right.charValue_;
-		this->intValue_ = right.intValue_;
-		this->dblValue_ = right.dblValue_;
-		this->fltValue_ = right.fltValue_;
-    }
     return *this;
 }
--- apps/ex06/ex00/ScalarConverter.hpp
+++ apps/ex06/ex00/ScalarConverter.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ScalarConverter.hpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: smorphet <smorphet@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: smorphet <smorphet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 13:02:32 by smorphet          #+#    #+#             */
-/*   Updated: 2023/10/11 13:02:38 by smorphet         ###   ########.fr       */
+/*   Updated: 2023/10/11 17:49:40 by smorphet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,43 +20,20 @@
 class ScalarConverter
 {
 	private:
-		static int		intValue_;
-		static float	fltValue_;
-		static char		charValue_;
-		static double	dblValue_;
-		static int		type_;
-		static bool		isPossible_; 
-		static int		isPseudo_; 
-		static void		convertToChar();
-		static void		convertToInt();
-		static void		convertToFlt();
-		static void		convertToDbl();
-		static void		setType(const std::string &input);
-		static bool		isInfOrNan(const std::string& input);
-		static void		initialConversion(const std::string &input);
-
 		ScalarConverter();
 		ScalarConverter(ScalarConverter const &src);
 		~ScalarConverter();
 		ScalarConverter	&operator=(ScalarConverter const &right);
 		
+
+	public:
+		static void	convert(const std::string &input);
 		class invalidType : public std::exception
 		{
 			public:
 				virtual const char* what() const throw();
 		};
-		enum e_pseudo_
-		{
-			NAN, POSINF, NEGINF, NOTPSEUDO
-		};
-		enum e_type_
-		{
-			CHAR, INT, FLOAT, DOUBLE, NOTYPE, NANINF
-		};
-
-	public:
-		static void	convert(const std::string &input);
-
+        
 
 };
 
