2eea1eb351493dc0ec259f9c665243456e3d34f9
Sheree <92857375+ShereeMorphett@users.noreply.github.com>
2023-10-17 20:46:28 +0300

    Update Iter.hpp
    adding the additional const template

--- cpp07_submission/ex01/Iter.hpp
+++ cpp07_submission/ex01/Iter.hpp
@@ -31,4 +31,13 @@ void iter(T* arr, int length, void (*f)( T & ))
     }
 }
 
+template <typename T>
+void iter(const T* arr, int length, void (*f)(const T&))
+{
+    for (int i = 0; i < length; ++i)
+    {
+        f(arr[i]);
+    }
+}
+
 #endif
