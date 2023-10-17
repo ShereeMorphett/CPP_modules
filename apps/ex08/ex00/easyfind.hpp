fc8f8175b8019ed42548406fec39e1e09912f687
ShereeAnne <92857375+ShereeAnne@users.noreply.github.com>
2023-10-17 16:18:03 +0300

    addition of cpp07 repo

---
+++ apps/ex08/ex00/easyfind.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smorphet <smorphet@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/17 16:07:46 by smorphet          #+#    #+#             */
+/*   Updated: 2023/10/17 16:10:32 by smorphet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <algorithm>
+
+class notFound : public std::exception
+{
+	public:
+		virtual const char* what() const throw();
+	
+}
+
+
+
+template<typename T>
+easyfind(T intCont, int toFind)
+{
+	throw()
+
+
+	
+}
